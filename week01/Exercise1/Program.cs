2ff3a193148f6d3c85970f7312e30e8454b1edbd
0J3J8A <juanjoaguirre38@gmail.com>
2025-07-01 18:32:10 -0500

    adding exercise one

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your First Name? ");
+        string FIRSTNAME = Console.ReadLine();
+        Console.WriteLine("What is your Last Name? ");
+        string LASTNAME = Console.ReadLine();
+        Console.WriteLine($"Your name is {LASTNAME}, {FIRSTNAME} {LASTNAME}.");
     }
 }
