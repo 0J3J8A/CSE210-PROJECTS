70f5a1a9408db2c37b2950f6ee9d59cf1f65de27
0J3J8A <juanjoaguirre38@gmail.com>
2025-07-01 19:39:45 -0500

    adding exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,27 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("This is the game guess the magic number game. Ready to start it.");
+        Random randomNumber = new Random();
+
+        int MAGICNUMBER = randomNumber.Next(1, 101);
+        Console.WriteLine("What is your guess?");
+        int GUESSNUMBER = int.Parse(Console.ReadLine());
+
+        while (GUESSNUMBER != MAGICNUMBER)
+        {
+            if (GUESSNUMBER < MAGICNUMBER)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (GUESSNUMBER > MAGICNUMBER)
+            {
+                Console.WriteLine("Lower");
+            }
+            Console.WriteLine("What is your guess?");
+            string INPUTNUMBER = Console.ReadLine();
+            GUESSNUMBER = int.Parse(INPUTNUMBER);
+        }
+        Console.WriteLine($"You guessed it. The magic number is: {MAGICNUMBER}.");
     }
 }
