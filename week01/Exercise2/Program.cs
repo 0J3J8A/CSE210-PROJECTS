b8d81787dc4e14ed9be59702f66b0329a466f9a7
0J3J8A <juanjoaguirre38@gmail.com>
2025-07-01 19:13:58 -0500

    adding excercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Please give me your grade percentage, I'm going to calculate your grade.");
+        string GRADEPERCENTAGE = Console.ReadLine();
+        int NUMBERPERCENTAGE = int.Parse(GRADEPERCENTAGE);
+
+        string LETTER = "";
+
+        if (NUMBERPERCENTAGE >= 90)
+        {
+            LETTER = "A";
+        }
+        else if (NUMBERPERCENTAGE >= 80)
+        {
+            LETTER = "B";
+        }
+        else if (NUMBERPERCENTAGE >= 70)
+        {
+            LETTER = "C";
+        }
+        else if (NUMBERPERCENTAGE >= 60)
+        {
+            LETTER = "D";
+        }
+        else
+        {
+            LETTER = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {LETTER}");
+        
+        if (NUMBERPERCENTAGE >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("I know that you can do it, the next time would be better.");
+        }     
     }
 }
