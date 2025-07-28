2637d9dd022a6393aa783e8c13bc9b300b913a98
0J3J8A <juanjoaguirre38@gmail.com>
2025-07-28 18:54:00 -0500

    adding the learning activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment assignment1 = new Assignment("Juan Jose Aguirre", "SUM");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment assignment2 = new MathAssignment("Juan Jose Aguirre", "SUM", "7.8", "3-10");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment assignment3 = new WritingAssignment("Juan Jose Aguirre", "WWII", "Why it was the worst war?");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
+        Console.WriteLine();
     }
 }
